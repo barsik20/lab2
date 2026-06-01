cb3ae0877b964a00604149040325a830397c7396
barsik20 <bars.070620077777@gmail.com>
2026-06-01 19:28:55 +0300

    Add documentation comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,13 @@
 
 int main()
 {
+    // Твоё имя
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
+    
+    // приветствие
     std::cout << "Hello world from " << name << std::endl;
     return 0;
+}return 0;
 }
